af73a2d74f2e371e4863b2fdf47b313d97b1e49a
AdaDavE <122479313+AdaDavE@users.noreply.github.com>
2023-10-19 12:17:42 -0600

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Hourglass
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
