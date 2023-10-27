7547a43714aaa93eb5d5160cf97337ecdd18cdf7
kaicottrell <kaicottrell7@gmail.com>
2023-10-27 15:39:03 -0600

    Added Modeling Functionality

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
 using Hourglass.Data;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(connectionString, providerOptions => providerOptions.EnableRetryOnFailure(maxRetryCount: 5,
+		maxRetryDelay: TimeSpan.FromSeconds(10),
+		errorNumbersToAdd: null)));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+//change to application use not identity
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>();
+//	.AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.AddScoped<IUnitofWork, UnitofWork>();
 
 var app = builder.Build();
 
@@ -18,6 +35,15 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//starts seeding
+//using (var scope = app.Services.CreateScope())
+//{
+//	var services = scope.ServiceProvider;
+//	var context = services.GetRequiredService<ApplicationDbContext>();
+//	context.Database.EnsureCreated();
+//	DbInitializer.Initialize(context);
+//}
 
 app.UseHttpsRedirection();
 
