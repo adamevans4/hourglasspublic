84da06345851dd8b6ff2c05569bd358ea58774bb
kaicottrell <kaicottrell7@gmail.com>
2023-10-27 16:05:40 -0600

    Infrastructure

---
+++ ApplicationCore/Models/Session.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Models
+{
+	public class Session
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{yyyy-MM-dd}")]
+		public DateTime SessionStart { get; set; }
+		[Required]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{yyyy-MM-dd}")]
+		public DateTime SessionEnd { get; set; }
+		public TimeSpan Duration { get; set; }
+		public int TemplateId { get; set; }
+		[ForeignKey("TemplateId")]
+		public virtual required Template Template { get; set; }
+		
+		
+
+	}
+}
---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Utility;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+		public DbSet<ApplicationUser>ApplicationUser { get; set; }
+		public DbSet<Session> Session { get; set; }
+		public DbSet<Template> Template { get; set; }
+		public DbSet<TemplateGroup> TemplateGroup { get; set; }
+		public DbSet<UserTemplate> UserTemplate { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			// This is where you can enter seed data
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+	
+}
