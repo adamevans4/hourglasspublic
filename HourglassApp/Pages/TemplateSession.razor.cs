c3eac63f9200d5fd35d0d8e24c60254e53050f6e
kaicottrell <kaicottrell7@gmail.com>
2023-12-04 16:30:25 -0700

    Added PastSessions, styling changes

--- HourglassApp/Pages/TemplateSession.razor.cs
+++ HourglassApp/Pages/TemplateSession.razor.cs
@@ -19,6 +19,8 @@ namespace HourglassApp.Pages
         private NavigationManager NavigationManager { get; set; } // Inject NavigationManager
         private DateTime StartTime { get; set; } = DateTime.Now;
         private DateTime EndTime { get; set; } = DateTime.Now;
+        private String TextColor { get; set; } = "text-white";
+        private String GradientEndColor { get; set; } = "#FFF";
         protected override void OnInitialized()
         {
             if (_unitofWork != null)
@@ -35,6 +37,13 @@ namespace HourglassApp.Pages
                 
                   
             }
+            //Template coloring:
+
+            var template = _unitofWork.Template.GetById(TemplateID);
+            bool isDark = IsDarkColor(template.TemplateColor);
+            TextColor = isDark ? "text-white" : "text-black";
+
+            GradientEndColor = GetTransitionColor(template.TemplateColor, isDark);
 
         }
         private DateTime GetRoundedToMinute(DateTime dateTime)
@@ -65,7 +74,7 @@ namespace HourglassApp.Pages
                 var CurrentSession = _unitofWork.Session.GetById(SessionID.Value);
                 CurrentSession.SessionStart = GetRoundedToMinute(StartTime);
                 CurrentSession.SessionEnd = GetRoundedToMinute(EndTime);
-                CurrentSession.Duration = EndTime.AddMinutes(1) - StartTime;
+                CurrentSession.Duration = EndTime - StartTime;
                 _unitofWork.Session.Update(CurrentSession);
                 _unitofWork.Commit();
                 NavigationManager.NavigateTo("/PastSessions/");
@@ -88,6 +97,45 @@ namespace HourglassApp.Pages
             }
 
 
+        }
+        //Gradient coloring
+        bool IsDarkColor(string color)
+        {
+            if (string.IsNullOrEmpty(color) || color.Length < 6 || !color.StartsWith("#"))
+            {
+                return false; // Return default value (considering the color invalid)
+            }
+
+            var hexColor = color.TrimStart('#');
+            if (hexColor.Length != 6)
+            {
+                return false; // Return default value (considering the color invalid)
+            }
+
+            var r = Convert.ToInt32(hexColor.Substring(0, 2), 16);
+            var g = Convert.ToInt32(hexColor.Substring(2, 2), 16);
+            var b = Convert.ToInt32(hexColor.Substring(4, 2), 16);
+
+            // Calculate the perceived brightness
+            var perceivedBrightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+            return perceivedBrightness < 70; // Adjust this threshold for what you consider "dark"
+        }
+
+
+        string GetTransitionColor(string color, bool isDark)
+        {
+            var originalColor = System.Drawing.ColorTranslator.FromHtml(color);
+            int delta = isDark ? 70 : -70; // Adjusted delta value for dark and light transitions
+            int r = Clamp(originalColor.R + delta);
+            int g = Clamp(originalColor.G + delta);
+            int b = Clamp(originalColor.B + delta);
+            return $"#{r:X2}{g:X2}{b:X2}";
+        }
+
+        int Clamp(int value)
+        {
+            return Math.Clamp(value, 0, 255); // Clamps the value between 0 and 255
         }
 
     }
