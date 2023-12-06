ae00a1b0a4e0c1e99e7fda045e3b3cf09e40f0cd
Jordan Greer <jwgreer2@gmail.com>
2023-12-05 23:25:18 -0700

    updated index page

--- HourGlass/Pages/Index.razor.cs
+++ HourGlass/Pages/Index.razor.cs
@@ -3,6 +3,7 @@ using Hourglass.Shared;
 using ApplicationCore.Models;
 using ApplicationCore.Interfaces;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace Hourglass.Pages
 {
@@ -11,6 +12,8 @@ namespace Hourglass.Pages
         [Inject]
         private IUnitofWork? _unitofWork { get; set; }
         private IEnumerable<Template>? TemplateList { get; set; }
+
+        private IEnumerable<Session>? SessionList { get; set; }
 
         [Inject]
         private NavigationManager Navigation { get; set; } // Inject NavigationManager
@@ -24,11 +27,19 @@ namespace Hourglass.Pages
             Navigation.NavigateTo("/UpsertTemplate/");
         }
 
+        
+
+
+
         protected override void OnInitialized()
         {
             // Gather all of the templates associated with the user
             // Can utilize a predicate function to gather the templates you want
             TemplateList = _unitofWork.Template.List();
+
+            SessionList = _unitofWork.Session.List();
+
+            
         }
     }
 }
