11a64638acc943970c4c8ee5954d78a5f98dd847
kaicottrell <kaicottrell7@gmail.com>
2023-12-05 23:48:06 -0700

    Starting Template Group

---
+++ HourGlass/Pages/TemplateGroupSetup.razor.cs
@@ -0,0 +1,23 @@
+using global::System;
+using global::System.Collections.Generic;
+using global::System.Linq;
+using global::System.Threading.Tasks;
+using global::Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using Hourglass;
+using Hourglass.Shared;
+
+namespace Hourglass.Pages
+{
+    public partial class TemplateGroupSetup
+    {
+
+    }
+}
